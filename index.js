aebd11eb4d7cc41db3ffc0fa15723f9e43da3dbe
Cayden <Caydenburr@gmail.com>
2023-02-13 18:44:04 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, eventName) {
+    const newArray =[];
+    for (let i = 0; i < array.length; i++) {
+       newArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    };  
+   return newArray;
+};
+
+function countDown(num){
+    for(let r = num; r >= 0; r--){
+        console.log(r);
+    };
+};
+
